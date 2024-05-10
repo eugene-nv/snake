06417a612008ae99355669864cf2d9d1947e40ee
eugene-nv <naumkin.e.v@yandex.ru>
2024-05-10 13:11:54 +0500

    make simple game loop

--- game_loop.py
+++ game_loop.py
@@ -2,3 +2,21 @@ import pygame
 
 pygame.init()
 
+window = pygame.display.set_mode((800, 600))
+
+'''Главный цикл игры'''
+
+game_over = False
+while not game_over:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+
+    window.fill((255, 255, 255))
+    pygame.display.update()
+
+pygame.quit()
+quit()
+
+
