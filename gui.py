a6ce01541741d3d227f17b631cc62671222de001
eugene-nv <naumkin.e.v@yandex.ru>
2024-05-11 14:24:03 +0500

    add name and icon for window

--- gui.py
+++ gui.py
@@ -9,6 +9,9 @@ class Window:
         self.background_color = (255, 255, 255)
         self.main_window = pygame.display.set_mode((self.width, self.height))
 
+        pygame.display.set_caption('Snake')
+        pygame.display.set_icon(pygame.image.load('icon.png'))
+
     def background_fill(self):
         return self.main_window.fill(self.background_color)
 
